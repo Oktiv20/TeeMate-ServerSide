a2d8230c9c95f8ae2379de0ead9b72e796554351
Kyle Blunt <kbluntaudio@gmail.com>
2023-11-13 18:35:17 -0600

    User endpoints complete

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using TeeMate_ServerSide;
 using TeeMate_ServerSide.Models;
-using Microsoft.EntityFrameworkCore;
-using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Http.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,5 +33,72 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+
+// USER ENDPOINTS
+
+
+// Create a User
+
+app.MapPost("/api/user", (TeeMateDbContext db, User user) =>
+{
+    db.Users.Add(user);
+    db.SaveChanges();
+    return Results.Created($"/api/user/{user.Id}", user);
+});
+
+
+// Get All Users
+
+app.MapGet("/api/users", (TeeMateDbContext db) =>
+{
+    List<User> users = db.Users.Include(t => t.TeeTimes).ToList();
+    if (users.Count == 0)
+    {
+        return Results.NotFound("No users to be found.");
+    }
+
+    return Results.Ok(users);
+});
+
+
+// Get User by uid
+
+app.MapGet("/api/user/{uid}", (TeeMateDbContext db, string uid) =>
+{
+    var user = db.Users.Single(u => u.Uid == uid);
+    return user;
+});
+
+
+// Update a User
+
+app.MapPut("/api/user/{id}", (TeeMateDbContext db, int id, User user) =>
+{
+    User UserToUpdate = db.Users.SingleOrDefault(user => user.Id == id);
+    if (UserToUpdate == null)
+    {
+        return Results.NotFound("No user available.");
+    }
+
+    UserToUpdate.FirstName = user.FirstName;
+    UserToUpdate.LastName = user.LastName;
+    UserToUpdate.Age = user.Age;
+    UserToUpdate.Handicap = user.Handicap;
+    UserToUpdate.Availability = user.Availability;
+    UserToUpdate.Transportation = user.Transportation;
+    UserToUpdate.Clubs = user.Clubs;
+    UserToUpdate.ProfilePic = user.ProfilePic;
+    UserToUpdate.Uid = user.Uid;
+    UserToUpdate.SkillLevelId = user.SkillLevelId;
+
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+
+// Delete A User
+
+
 
 app.Run();
